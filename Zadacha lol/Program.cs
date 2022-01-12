9d9335ffaa433cc66637fdf50bbff062434bfcb4
georgisg99@gmail.com <georgisg99@gmail.com>
2022-01-12 10:57:09 +0200

    Add project files.

---
+++ Zadacha lol/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Zadacha_lol
+{
+    class Program
+    {
+        static void Main(string[] args)
+        { 
+            
+                int[] IntArray = new int[10] { 1, 3, 5, 7, 11, 13, 17, 19, 23, 31 };
+                int target = 17;
+                int pos = Array.BinarySearch(IntArray, target);
+                if (pos >= 0)
+                    Console.WriteLine($"Item {IntArray[pos].ToString()} found at position {pos + 1}.");
+                else
+                    Console.WriteLine("Item not found");
+                Console.ReadLine();
+            
+        }
+    }
+}
---
+++ Zadacha lol/ProgramBase.cs
@@ -0,0 +1,23 @@
+﻿namespace Zadacha_lol
+{
+    internal class ProgramBase
+    {
+        static bool Main(int n)
+        {
+
+            n = int.Parse(Console.ReadLine());
+
+            if (n <= 1)
+                return false;
+
+            for (int i = 2; i < n; i++)
+            {
+                if (n % i == 0)
+                    return false;
+
+                return true;
+
+            }
+        }
+    }
+}
